2d7433c913a2cb9cd8772b9ad101569520ee0e2a
ludoff <ludovicofiore25@gmail.com>
2024-07-25 16:17:34 +0200

    feat: add basic layout with form

--- index.php
+++ index.php
@@ -31,6 +31,40 @@ $caratteri_speciali = ['!', '?', '&', '%', '$', '<', '>', '^', '+', '-', '*', '/
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+
+<div class="container-fluid">
+    <div class="col-12 text-center mb-5">
+        <h1>Strong Password Generator</h1>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-8 text-center border border-primary">
+            <h3>Genera una password compresa tra 8 e 32 caratteri</h3>
+        </div>
+
+        <div class="col-8 border border-dark">
+
+            <form action="index.php" method="get">
+                <!-- filtri -->
+                <div class="col-12 p-5">
+                    <label for="numero-caratteri">Lunghezza password:</label>
+
+                    <input type="number" id="numero-caratteri" name="numero-caratteri" min="8" max="32" />
+                </div>
+
+                <!-- bottoni -->
+                <div class="col-5 p-4">
+                    <button type="submit" class="btn btn-primary">Crea</button>
+                    <button type="reset" class="btn btn-secondary">Annulla</button>
+                </div>
+
+            </form>
+            
+            
+        </div>
+
+    </div>
+</div>
    
 
 
